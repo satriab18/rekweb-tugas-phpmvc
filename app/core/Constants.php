9a6a36a13690e37ce7d732392fde31e876d0304d
Satria Bagus <satria.bagus18@gmail.com>
2019-10-03 20:47:41 +0700

    menambahkan assets dan membuat pemanggilan URL yang disingkat dengan Constant

---
+++ app/core/Constants.php
@@ -0,0 +1,6 @@
+<?php
+
+define('BASEURL', 'http://127.0.0.1/Tugas_Kuliah/RekWeb/php-mvc/public/');
+
+
+?>
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>Halaman About</h1>
+<div class="container">
+    <h1 class="mt-4">Halaman About</h1>
+    <img src="<?=BASEURL; ?>/img/profile.jpg" alt="Satria Bagus" class="rounded mx-auto d-block shadow" width=250;>
     <p>Halo, nama saya <?= $data['nama']?>, Umur saya <?= $data['umur']?> tahun, saya seorang <?= $data['pekerjaan']?></p>
+</div>
